35f61079862db46dd249cd391e590607d6a2493a
Austin Smith <austindsmith_accounts@proton.me>
2026-06-28 11:47:38 -0600

    migrated credentials to 1 password. wayyyyyyy less tedious than 50 sops files

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -1,50 +1,60 @@
 terraform {
-  # Utilizes .envrc to pull from encrypted secret.yaml.
   backend "local" { path = "../../.tfstate/cloudflare/terraform.tfstate" }
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "5.21.1"
     }
-    # Allows SOPs decryption of secrets.
-    sops = {
-      source  = "carlpett/sops"
-      version = "~> 1.4"
-    }
-    # Allows access to local file system.
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.9.0"
+    onepassword = {
+      source  = "1Password/onepassword"
+      version = "~> 2.1"
     }
   }
 }
 
-data "sops_file" "cloudflare" {
-  source_file = "${path.module}/secret.yaml"
+provider "onepassword" {
+  account = "my.1password.com"
+}
+
+data "onepassword_item" "cloudflare" {
+  vault = "the-white-lodge"
+  title = "cloudflare"
+}
+
+data "onepassword_item" "netcup" {
+  vault = "the-white-lodge"
+  title = "netcup"
+}
+
+locals {
+  cf_api_token  = data.onepassword_item.cloudflare.field["api_token"]
+  cf_account_id = data.onepassword_item.cloudflare.field["account_id"]
+  domain        = data.onepassword_item.netcup.field["domain"]
+  email_address = data.onepassword_item.cloudflare.field["username"]
 }
 
 provider "cloudflare" {
-  api_token = data.sops_file.cloudflare.data["cloudflare_api_token"]
+  api_token = local.cf_api_token
 }
 
 resource "cloudflare_zero_trust_access_policy" "example_zero_trust_access_policy" {
-  account_id = data.sops_file.cloudflare.data["cloudflare_account_id"]
+  account_id = local.cf_account_id
   decision   = "allow"
   name       = "Email verification"
   include = [{
     email = {
-      email = "${data.sops_file.cloudflare.data["email_address"]}"
+      email = "${local.email_address}"
     }
   }]
   session_duration = "24h"
 }
 
 resource "cloudflare_zero_trust_access_application" "wildcard" {
-  account_id                 = data.sops_file.cloudflare.data["cloudflare_account_id"]
+  account_id                 = local.cf_account_id
   allowed_idps               = []
   app_launcher_visible       = true
   auto_redirect_to_identity  = false
-  domain                     = "*.${data.sops_file.cloudflare.data["domain"]}"
+  domain                     = "*.${local.domain}"
   enable_binding_cookie      = false
   http_only_cookie_attribute = false
   name                       = "*"
@@ -54,8 +64,62 @@ resource "cloudflare_zero_trust_access_application" "wildcard" {
   type                       = "self_hosted"
   destinations = [{
     type = "public"
-    uri  = "*.${data.sops_file.cloudflare.data["domain"]}"
+    uri  = "*.${local.domain}"
   }]
+  policies = [{
+    id         = cloudflare_zero_trust_access_policy.example_zero_trust_access_policy.id
+    precedence = 1
+  }]
+}
+
+locals {
+  passthrough_apps = toset([
+    "authentik",
+    "rd-id",
+    "rd-relay",
+    "rustdesk",
+    "teleport",
+  ])
+}
+
+resource "cloudflare_zero_trust_access_application" "passthrough" {
+  for_each = local.passthrough_apps
+
+  account_id       = local.cf_account_id
+  name             = "${title(each.key)} passthrough"
+  domain           = "${each.key}.${local.domain}"
+  session_duration = "24h"
+  type             = "self_hosted"
+
+  destinations = [{
+    type = "public"
+    uri  = "${each.key}.${local.domain}"
+  }]
+
+  policies = [{
+    decision   = "bypass"
+    name       = "Passthrough"
+    include    = [{ everyone = {} }]
+    precedence = 1
+    require    = []
+    reusable   = true
+  }]
+}
+
+resource "cloudflare_zero_trust_access_application" "authentik_admin_console" {
+  account_id                = local.cf_account_id
+  app_launcher_visible      = false
+  auto_redirect_to_identity = false
+  name                      = "Authentik admin console"
+  session_duration          = "24h"
+  type                      = "self_hosted"
+  domain                    = "authentik.${local.domain}/if/admin/*"
+
+  destinations = [{
+    type = "public"
+    uri  = "authentik.${local.domain}/if/admin/*"
+  }]
+
   policies = [{
     id         = cloudflare_zero_trust_access_policy.example_zero_trust_access_policy.id
     precedence = 1
--- terraform/unifi/main.tf
+++ terraform/unifi/main.tf
@@ -1,6 +1,14 @@
 terraform {
   backend "local" { path = "../../.tfstate/unifi/terraform.tfstate" }
   required_providers {
+    ansible = {
+      source  = "ansible/ansible"
+      version = "1.4.0"
+    }
+    onepassword = {
+      source  = "1Password/onepassword"
+      version = "3.3.1"
+    }
     unifi = {
       source  = "ubiquiti-community/unifi"
       version = "0.53.0"
@@ -9,28 +17,44 @@ terraform {
       source  = "OJFord/wireguard"
       version = "0.4.0"
     }
-    ansible = {
-      source  = "ansible/ansible"
-      version = "1.4.0"
-    }
-    sops = {
-      source  = "carlpett/sops"
-      version = "~> 1.4"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.9.0"
-    }
   }
 }
 
-data "sops_file" "unifi" {
-  source_file = "${path.module}/secret.yaml"
+provider "onepassword" {
+  account = "my.1password.com"
+}
+
+data "onepassword_item" "ansible" {
+  vault = "the-white-lodge"
+  title = "ansible"
+}
+
+data "onepassword_item" "netcup" {
+  vault = "the-white-lodge"
+  title = "netcup"
+}
+
+data "onepassword_item" "unifi" {
+  vault = "the-white-lodge"
+  title = "unifi"
+}
+
+locals {
+  ansible = data.onepassword_item.ansible.section_map[""].field_map
+
+  netcup = data.onepassword_item.netcup.section_map[""].field_map
+
+  unifi = data.onepassword_item.unifi.section_map[""].field_map
+
+}
+
+output "debug_ansible_fields" {
+  value = keys(data.onepassword_item.ansible.section_map[""].field_map)
 }
 
 provider "unifi" {
-  api_url        = data.sops_file.unifi.data["unifi_api_url"]
-  api_key        = data.sops_file.unifi.data["unifi_api_key"]
+  api_url        = local.unifi["api_url"].value
+  api_key        = local.unifi["api_key"].value
   allow_insecure = true
 }
 
@@ -39,39 +63,39 @@ provider "wireguard" {}
 resource "wireguard_asymmetric_key" "netcup" {}
 
 resource "unifi_wireguard_peer" "netcup" {
-  network_id   = data.sops_file.unifi.data["wg_server_id"]
+  network_id   = local.unifi["wg_server_id"].value
   name         = "netcup"
-  interface_ip = data.sops_file.unifi.data["wg_interface_ip"]
+  interface_ip = local.netcup["wg_ip_address"].value
   public_key   = wireguard_asymmetric_key.netcup.public_key
 }
 
 resource "ansible_playbook" "wireguard" {
   playbook   = "${path.cwd}/../../ansible/playbooks/wireguard.yml"
-  name       = data.sops_file.unifi.data["netcup_host"]
+  name       = local.netcup["public_ip_address"].value
   replayable = true
   extra_vars = {
-    wg_interface_ip              = data.sops_file.unifi.data["wg_interface_ip"]
-    wg_server_endpoint           = data.sops_file.unifi.data["wg_server_endpoint"]
-    wg_server_pubkey             = data.sops_file.unifi.data["wg_server_pubkey"]
-    wg_listen_port               = data.sops_file.unifi.data["wg_listen_port"]
+    wg_interface_ip              = local.netcup["wg_ip_address"].value
+    wg_server_endpoint           = local.unifi["public_ip_address"].value
+    wg_server_pubkey             = local.unifi["wg_server_pub_key"].value
+    wg_listen_port               = local.unifi["wg_listen_port"].value
     wg_private_key               = wireguard_asymmetric_key.netcup.private_key
-    ansible_user                 = data.sops_file.unifi.data["ansible_user"]
-    ansible_become_user          = data.sops_file.unifi.data["ansible_become_user"]
-    ansible_become_password      = data.sops_file.unifi.data["ansible_become_password"]
-    ansible_ssh_private_key_file = data.sops_file.unifi.data["ansible_ssh_private_key_file"]
+    ansible_user                 = data.onepassword_item.ansible.username
+    ansible_become_user          = local.ansible["become_user"].value
+    ansible_become_password      = data.onepassword_item.ansible.password
+    ansible_ssh_private_key_file = local.ansible["ssh_private_key_file"].value
   }
   depends_on = [unifi_wireguard_peer.netcup]
 }
 
 resource "ansible_playbook" "site" {
   playbook   = "${path.cwd}/../../ansible/playbooks/kubernetes.yml"
-  name       = data.sops_file.unifi.data["netcup_host"]
+  name       = local.netcup["public_ip_address"].value
   replayable = true
   extra_vars = {
-    ansible_user                 = data.sops_file.unifi.data["ansible_user"]
-    ansible_become_user          = data.sops_file.unifi.data["ansible_become_user"]
-    ansible_become_password      = data.sops_file.unifi.data["ansible_become_password"]
-    ansible_ssh_private_key_file = data.sops_file.unifi.data["ansible_ssh_private_key_file"]
+    ansible_user                 = data.onepassword_item.ansible.username
+    ansible_become_user          = local.ansible["become_user"].value
+    ansible_become_password      = data.onepassword_item.ansible.password
+    ansible_ssh_private_key_file = local.ansible["ssh_private_key_file"].value
   }
   depends_on = [ansible_playbook.wireguard]
 }
