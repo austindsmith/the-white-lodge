8ab415de0dbad98ff607851d1035f67baa0c7b0e
Austin Smith <austindsmith_accounts@proton.me>
2026-07-06 21:45:05 -0600

    fixing plex porting

--- terraform/unifi/main.tf
+++ terraform/unifi/main.tf
@@ -1,10 +1,6 @@
 terraform {
   backend "local" { path = "../../.tfstate/unifi/terraform.tfstate" }
   required_providers {
-    ansible = {
-      source  = "ansible/ansible"
-      version = "1.4.0"
-    }
     onepassword = {
       source  = "1Password/onepassword"
       version = "3.3.1"
@@ -24,9 +20,8 @@ provider "onepassword" {
   account = "my.1password.com"
 }
 
-data "onepassword_item" "ansible" {
-  vault = "the-white-lodge"
-  title = "ansible"
+data "onepassword_vault" "white_lodge" {
+  name = "the-white-lodge"
 }
 
 data "onepassword_item" "netcup" {
@@ -40,16 +35,8 @@ data "onepassword_item" "unifi" {
 }
 
 locals {
-  ansible = data.onepassword_item.ansible.section_map[""].field_map
-
   netcup = data.onepassword_item.netcup.section_map[""].field_map
-
-  unifi = data.onepassword_item.unifi.section_map[""].field_map
-
-}
-
-output "debug_ansible_fields" {
-  value = keys(data.onepassword_item.ansible.section_map[""].field_map)
+  unifi  = data.onepassword_item.unifi.section_map[""].field_map
 }
 
 provider "unifi" {
@@ -69,44 +56,46 @@ resource "unifi_wireguard_peer" "netcup" {
   public_key   = wireguard_asymmetric_key.netcup.public_key
 }
 
-resource "ansible_playbook" "wireguard" {
-  playbook   = "${path.cwd}/../../ansible/playbooks/wireguard.yml"
-  name       = local.netcup["public_ip_address"].value
-  replayable = true
-  extra_vars = {
-    wg_interface_ip              = local.netcup["wg_ip_address"].value
-    wg_server_endpoint           = local.unifi["public_ip_address"].value
-    wg_server_pubkey             = local.unifi["wg_server_pub_key"].value
-    wg_listen_port               = local.unifi["wg_listen_port"].value
-    wg_private_key               = wireguard_asymmetric_key.netcup.private_key
-    ansible_user                 = data.onepassword_item.ansible.username
-    ansible_become_user          = local.ansible["become_user"].value
-    ansible_become_password      = data.onepassword_item.ansible.password
-    ansible_ssh_private_key_file = local.ansible["ssh_private_key_file"].value
-  }
-  depends_on = [unifi_wireguard_peer.netcup]
-}
+resource "onepassword_item" "wireguard_netcup" {
+  vault    = data.onepassword_vault.white_lodge.uuid
+  title    = "wireguard-netcup"
+  category = "password"
 
-resource "ansible_playbook" "site" {
-  playbook   = "${path.cwd}/../../ansible/playbooks/kubernetes.yml"
-  name       = local.netcup["public_ip_address"].value
-  replayable = true
-  extra_vars = {
-    ansible_user                 = data.onepassword_item.ansible.username
-    ansible_become_user          = local.ansible["become_user"].value
-    ansible_become_password      = data.onepassword_item.ansible.password
-    ansible_ssh_private_key_file = local.ansible["ssh_private_key_file"].value
+  section {
+    label = "wireguard"
+
+    field {
+      label = "public_key"
+      type  = "STRING"
+      value = wireguard_asymmetric_key.netcup.public_key
+    }
+
+    field {
+      label = "private_key"
+      type  = "CONCEALED"
+      value = wireguard_asymmetric_key.netcup.private_key
+    }
+
+    field {
+      label = "interface_ip"
+      type  = "STRING"
+      value = local.netcup["wg_ip_address"].value
+    }
+
+    field {
+      label = "server_endpoint"
+      type  = "STRING"
+      value = "${local.unifi["public_ip_address"].value}:${local.unifi["wg_listen_port"].value}"
+    }
+
+    field {
+      label = "server_public_key"
+      type  = "STRING"
+      value = local.unifi["wg_server_pub_key"].value
+    }
   }
-  depends_on = [ansible_playbook.wireguard]
 }
 
 output "wg_public_key" {
-  description = "netcup public WireGuard key"
-  value       = wireguard_asymmetric_key.netcup.public_key
-}
-
-output "wg_private_key" {
-  description = "netcup private WireGuard key"
-  value       = wireguard_asymmetric_key.netcup.private_key
-  sensitive   = true
+  value = wireguard_asymmetric_key.netcup.public_key
 }
